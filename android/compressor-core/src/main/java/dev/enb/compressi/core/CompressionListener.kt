7c184835a36446e704fe23e3c092abd7db42b022
Mohamad Ilham <mohamadilham25@gmail.com>
2026-06-11 08:20:52 +0700

    chore(android): remove stale enb namespace

--- android/compressor-core/src/main/java/dev/enb/compressi/core/CompressionListener.kt
+++
@@ -1,8 +0,0 @@
-package dev.enb.compressi.core
-
-interface CompressionListener {
-    fun onStateChanged(state: CompressionState)
-    fun onSuccess(result: CompressionSuccess)
-    fun onFailure(failure: CompressionFailure)
-}
-
